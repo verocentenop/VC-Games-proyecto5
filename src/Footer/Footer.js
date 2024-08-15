39812198de4fbfa261fdca6ea66aad36f7f02bf4
verocentenop <159185910+verocentenop@users.noreply.github.com>
2024-08-15 18:12:25 +0200

    Update Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -6,6 +6,10 @@ export const printFooter = () => {
   const linkedin = document.createElement('img')
   linkedin.className = 'linkedin'
   linkedin.src = 'https://cdn-icons-png.flaticon.com/128/3536/3536505.png'
-  footer.append(p, linkedin)
+  const link = document.createElement('a')
+  link.href = 'https://www.linkedin.com/in/veronica-centeno-a7222b1a1/'
+  link.append(linkedin)
+  
+  footer.append(p, link)
   appContainer.append(footer)
 }
